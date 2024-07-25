2275f528e592148b0097ad3ea59a6815c2c8dbc4
Farzeena P A <farzinaayub266@gmail.com>
2024-07-25 13:41:47 +0530

    First draft of Blog generator

--- app.py
+++ app.py
@@ -129,5 +129,9 @@ def index():
     
     return render_template("index.html", result_str=Markdown(result_str).data)
 
+# if __name__ == "__main__":
+#     app.run(debug=False)
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
